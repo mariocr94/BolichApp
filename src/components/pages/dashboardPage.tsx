8f42567cd305acb4ee8c6a11da3d4821fcb08058
Mario Carrillo <mario.carrillo@clearcheck.us>
2023-02-12 14:06:44 -0600

    finish 1st pages refactor

--- src/components/pages/dashboardPage.tsx
+++ src/components/pages/dashboardPage.tsx
@@ -1,5 +1,23 @@
-const DashboardPage = () => {
-   return <div>dashboard</div>;
+import { IGames } from '@models/games';
+
+export interface DashboardPageProps {
+   games: IGames[];
+}
+
+const DashboardPage = ({ games }: DashboardPageProps) => {
+   const averageScore =
+      games.length > 0
+         ? (
+              games.reduce((sum, game) => {
+                 return (sum += game.score);
+              }, 0) / games.length
+           ).toFixed(2)
+         : '';
+   return (
+      <div className="container mx-auto flex h-to-fit flex-col items-center gap-4 p-4">
+         <h1>Current Average: {averageScore}</h1>
+      </div>
+   );
 };
 
 export default DashboardPage;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,27 @@
-import DashboardPage from '@components/pages/dashboardPage';
+import { TABLES } from '@common/constants/tables';
+import DashboardPage, { DashboardPageProps } from '@components/pages/dashboardPage';
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
-const Dashboard = () => {
-   return <DashboardPage />;
+const Dashboard = ({ games }: DashboardPageProps) => {
+   return <DashboardPage games={games} />;
 };
 
 export default Dashboard;
+
+export async function getServerSideProps(ctx) {
+   const supabase = createServerSupabaseClient(ctx);
+   const {
+      data: { session },
+   } = await supabase.auth.getSession();
+   if (!session)
+      return {
+         redirect: {
+            destination: '/',
+            permanent: false,
+         },
+      };
+
+   const { data: games } = await supabase.from(TABLES.GAMES).select('*');
+
+   return { props: { session, games } };
+}
