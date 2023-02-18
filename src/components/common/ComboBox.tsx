98e3f1d95fd6e7356d0a728fd989695e3ac4f305
Mario Carrillo <mario.carrillo@clearcheck.us>
2023-02-18 14:41:39 -0600

    z-index for combobox options

--- src/components/common/ComboBox.tsx
+++ src/components/common/ComboBox.tsx
@@ -54,7 +54,7 @@ const ComboBox = ({
                   leaveTo="opacity-0"
                   afterLeave={() => setQuery('')}
                >
-                  <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                      {data === null || (data.length === 0 && query !== '') ? (
                         <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                            Nothing found.
